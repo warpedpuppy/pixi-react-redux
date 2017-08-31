f913513d258ce2c9cf98e3d0290fb170f8f2a3e7
ted <ted@warpedpuppy.com>
2017-08-31 16:36:10 -0700

    asdf

--- src/components/home/HomeCanvas.js
+++ src/components/home/HomeCanvas.js
@@ -26,9 +26,10 @@ export default class HomeCanvas extends React.Component {
 	   app.ticker.add(function(delta) {
 		    
 		     for(let i = 0; i < that.helperQ; i ++){
-				    that.helpers[i].rotation += 0.05 * delta;
+			     	if(that.helpers[i].move === true){
+					    that.helpers[i].sprite.rotation += 0.05 * delta;
+					}
 				}
-
 		});
 
 	   var manager = new PIXI.interaction.InteractionManager(app.stage, app.view);
@@ -42,9 +43,9 @@ export default class HomeCanvas extends React.Component {
 	   		this.width = new_width;
 	   		this.app.renderer.resize(new_width, 150);
 	   		for(let i = 0; i < this.helperQ; i ++){
-				    this.helpers[i].x = _.random(0, this.props.resize.homeCanvasWidth);
+	   				this.helpers[i].x = _.random(0, this.props.resize.homeCanvasWidth);
 					this.helpers[i].y = _.random(0, 150);
-				}
+	   			}
 	   	}
    }
    add_helper(q){
@@ -55,22 +56,37 @@ export default class HomeCanvas extends React.Component {
 		   		h = new this.Helper();
 				h.x = _.random(0, this.props.resize.homeCanvasWidth);
 				h.y = _.random(0, 150);
-				h.anchor.x = h.anchor.y = .5;
-				h.scale.x = h.scale.y = _.random(0.25, 1, true);
+				h.sprite.scale.x = h.sprite.scale.y = _.random(0.25,1, true);
 				h.interactive = true;
 			    h.buttonMode = true;
-				h.on("click", this.Alter)
+				h.on("click", this.Alter.bind(this))
 				this.helpers.push(h);
 				this.app.stage.addChild(h);
 	   	}
    }
    Alter(e){
-   	console.log(e.target);
+   	var item = e.target;
+   	item.move = false;
+   	var midwidth = this.props.resize.homeCanvasWidth/2;
+   	TweenLite.to(item, 1, {x:midwidth, y:80});
+   	TweenLite.to(item.sprite.scale, 1, {x:1, y:1});
+   	TweenLite.to(item.sprite, 1, {x:1, y:1, rotation:0});
+
    }
 
    Helper(){
+   		let cont = new PIXI.Container();
 		let sprite = new PIXI.Sprite.fromImage('/media/helper.png');
-       	return sprite;
+		let text = new PIXI.Text("asdf");
+		text.anchor.x = .5;
+		text.y = 25;
+		sprite.anchor.x = sprite.anchor.y = .5;
+		cont.addChild(sprite);
+		cont.addChild(text);
+		cont.text = text;
+		cont.sprite = sprite;
+		cont.move = true;
+       	return cont;
    }
   
 	render(){
